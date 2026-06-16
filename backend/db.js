a4b3837577f511102b01e6f3853bd3151869dc98
Priyanshu Samanta <priyanshusamanta23@gmail.com>
2026-06-17 01:19:30 +0530

    prima fix

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { PrismaClient } from './generated/prisma/client.ts'
+import { PrismaClient } from './generated/prisma/client.js'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { env } from "prisma/config";
 
